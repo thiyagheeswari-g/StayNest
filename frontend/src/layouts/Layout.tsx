bff9854230478caf01323db0b77b8800b67c54b4
THIYAGHEESWARI G <thiyagheeswarig@gmail.com>
2024-07-24 10:29:15 +0530

    Create Layout.tsx

---
+++ frontend/src/layouts/Layout.tsx
@@ -0,0 +1,13 @@
+const Layout = ({ children }: Props) => {
+    return (
+      <div className="flex flex-col min-h-screen">
+        <Header />
+        <Hero />
+        <div className="container mx-auto">
+          <SearchBar />
+        </div>
+        <div className="container mx-auto py-10 flex-1">{children}</div>
+        <Footer />
+      </div>
+    );
+  };
